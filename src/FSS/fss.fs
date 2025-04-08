0130cf6c331f88f6ec77214f3928b22254ed35e9
gcannata <gcannata@gmail.com>
2025-04-08 19:25:41 +0200

    refactor: Update CssClassBuilder to accept a selector parameter and adjust usage in rules

--- src/FSS/fss.fs
+++ src/FSS/fss.fs
@@ -4,37 +4,37 @@ open FreeAct
 open Fable.Core.JS
 open System.Text.RegularExpressions
 
-type CssClassBuilder() =
+type CssClassBuilder(selector: string) =
     inherit StyleBuilder()
 
-    member inline _.Run(props: HtmlProp list) : HtmlProp list =
-
-        let ret = props
+    member inline _.Run(props: HtmlProp list) =
+        console.log ("CssClassBuilder", selector, props)
+        let ret = selector, props
         ret
 
-let css = CssClassBuilder()
+let css = CssClassBuilder
 
 let rules =
     // css "root" {
-    css {
+    css "root" {
         flex
         backgroundColor "#f0f0f0"
     }
 // }
 
 let rules2 =
-    {|
-        main =
-            css {
-                flex
-                backgroundColor "#f0f0f0"
-            }
-        header =
-            css {
-                flex
-                backgroundColor "#f0f0f0"
-            }
-    |}
+    [
+
+        css "main" {
+            flex
+            backgroundColor "#f0f0f0"
+        }
+
+        css "header" {
+            flex
+            backgroundColor "#f0f0f0"
+        }
+    ]
 
 let uppercaseRegex = Regex("([A-Z])", RegexOptions.Compiled)
 
@@ -55,7 +55,7 @@ let toCss (selector: string) (props: HtmlProp list) =
 
 let testCssInFsharp () =
     console.log ("rules", rules)
-    let cssText = toCss "test" rules
-    console.log ("cssText\n", cssText)
+    // let cssText = toCss "test" rules
+    // console.log ("cssText\n", cssText)
 
     console.log ("rules2", rules2)
